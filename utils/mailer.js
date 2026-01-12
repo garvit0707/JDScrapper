258a49a9a8c7361d51d4b036af5c5f7359fa9886
Garvit <garvit.sharma.0707@gmail.com>
2026-01-12 17:42:31 +0530

    generic scrapper logic

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,22 +1,22 @@
-const nodemailer = require("nodemailer")
-
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-         user: process.env.EMAIL_USER, // your email
-    pass: process.env.EMAIL_PASS, 
-    }
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.EMAIL_USER, // your email
+    pass: process.env.EMAIL_PASS, // Use app password if 2FA is enabled
+  },
 });
 
-
-exports.sendJobEmail = async(htmlContent) =>{
-   const mailOptions = {
+exports.sendJobEmail = async (htmlContent) => {
+  const mailOptions = {
     from: `"Job Scraper" <${process.env.EMAIL_USER}>`,
     to: process.env.RECEIVER_EMAIL,
-    subject: "🔥 Today's React Job Openings",
+    subject: "Today's React Native Job Openings",
     html: htmlContent,
   };
 
-  await transporter.sendMail(mailOptions)
-}
+  await transporter.sendMail(mailOptions);
+};
